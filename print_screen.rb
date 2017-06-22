1171583298425dde76c41521fd5ade029a8cb94c
Derrick Gillespie <derrickgillespiewv@gmail.com>
2017-06-22 11:46:33 -0400

    Ex:4 mod3=mined, mod5=minds, mod15=mined_minds

--- print_screen.rb
+++ print_screen.rb
@@ -1,10 +1,12 @@
 	x=0
 	while x<100
 	x+=1
-    if x % 3 == 0 
-    	p "mined"
-    elsif x == 5
+    if x % 15 == 0 
+    	p "mined_minds"
+    elsif x % 5 == 0
     	p "minds"
+    elsif x % 3 == 0
+    	p "mined"
     else p  x
 end
 end
