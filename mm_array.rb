446d8f2f18cf531c31b55db5750d4b253ab23343
Derrick Gillespie <derrickgillespiewv@gmail.com>
2017-07-07 15:50:38 -0400

    Array Test v 1

--- mm_array.rb
+++ mm_array.rb
@@ -1,4 +1,14 @@
-def kata()
+def kata
 	kata = [*1..100]
+	kata.each_with_index do |num,index|
+		if num % 15 == 0
+			 kata[index] = "mined_minds"
+		elsif num % 3 == 0
+			 kata[index] = "mined"
+		elsif num % 5 == 0
+			 kata[index] = "minds"
+		else 
+			 num
+		end
+	end
 end
-kata
--- mm_array_test.rb
+++ mm_array_test.rb
@@ -7,8 +7,40 @@ class TestMinedMinds < Minitest::Test
 	end
 
 	def test_count_100
-	results = kata
-	assert_equal(100, results.count)
-	assert_equal(Array, results.class)
-	end 
+		results = kata
+		assert_equal(100, results.count)
+		assert_equal(Array, results.class)
+	end
+
+	def test_first_number
+		assert_equal(1, kata[0]) 
+	end
+
+	def test_third_number
+		assert_equal("mined", kata[2])
+	end
+
+	def test_6_number
+		assert_equal("mined", kata[5])
+	end
+
+	def test_5_number
+		assert_equal("minds", kata[4])
+	end
+
+	def test_10_number
+		assert_equal("minds", kata[9])
+	end
+
+	def test_15_number
+		assert_equal("mined_minds", kata[14])
+	end
+
+	def test_30_number
+		assert_equal("mined_minds", kata[29])
+	end
+
+	def test_29_number
+		assert_equal(29, kata[28])
+	end
 end
